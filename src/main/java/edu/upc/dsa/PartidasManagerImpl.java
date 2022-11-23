23b622347d946349eac041cfc1cb293618b8d281
CarlaVictorio <101456362+CarlaVictorio@users.noreply.github.com>
2022-11-23 11:16:01 +0100

    partida class + managers

--- src/main/java/edu/upc/dsa/PartidasManagerImpl.java
+++ src/main/java/edu/upc/dsa/PartidasManagerImpl.java
@@ -1,5 +1,86 @@
 package edu.upc.dsa;
 
+import edu.upc.dsa.models.Partida;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class PartidasManagerImpl implements PartidasManager {
 
+    private static PartidasManager instance;
+    protected List<Partida> partidasList;
+
+    final static Logger logger = Logger.getLogger(String.valueOf(PartidasManagerImpl.class)); //està bé???
+    private PartidasManagerImpl() {
+        this.partidasList = new LinkedList<>();
+    }
+    public static PartidasManager getInstance(){
+        if (instance==null) instance = new PartidasManagerImpl();
+        return instance;
+    }
+    public int size(){
+        int ret = this.partidasList.size();
+        logger.info("size" + ret);
+        return ret;
+    }
+
+    public List<Partida> getAllPartidas(){
+        return this.partidasList;
+    }
+    /*
+    public List<Partida> getAllPartidasJugador(int idJugador){
+
+    }
+    public List<Partida> getAllPartidasMapa(int idMapa){
+
+    }
+    */
+
+    public Partida getPartida(int idPartida){
+        logger.info("getPartida("+idPartida+")");
+
+        for (Partida p: this.partidasList) {
+            if (p.getIdPartida()==idPartida) {
+                logger.info("getPartida("+idPartida+"): "+p);
+
+                return p;
+            }
+        }
+        logger.warning("not found " + idPartida);
+        return null;
+    }
+    public Partida addPartida(Partida p){
+        logger.info("new Partida " + p);
+        this.partidasList.add(p);
+        logger.info("new Partida added");
+        return p;
+    }
+    public Partida addPartida (int idPartida, int nivelActual, int puntos, int idMapa, int idJugador){
+        return this.addPartida(new Partida(idPartida,nivelActual, puntos,idMapa,idJugador));
+    }
+    @Override
+    public void deletePartida(int idPartida){
+        Partida p = this.getPartida(idPartida);
+        if (p==null){
+            logger.warning("not found " + p);
+        }
+        else logger.info(p + "deleted");
+        this.partidasList.remove(p);
+    }
+    @Override
+    public Partida updatePartida(Partida t){
+        Partida p = this.getPartida(t.getIdPartida());
+        if(p!=null){
+            logger.info(t+" received");
+            p.setIdPartida(t.getIdPartida());
+            p.setIdMapa(t.getIdMapa());
+            p.setPuntos(t.getPuntos());
+            p.setNivelActual(t.getNivelActual());
+            p.setIdJugador(t.getIdJugador());
+            logger.info(p + "updated");
+        }
+        else logger.warning("not found " +t);
+        return p;
+    }
 }
--- src/main/java/edu/upc/dsa/models/Partida.java
+++ src/main/java/edu/upc/dsa/models/Partida.java
@@ -1,9 +1,66 @@
 package edu.upc.dsa.models;
+import edu.upc.dsa.util.RandomUtils;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class Partida {
 
+    int idPartida;
+    int nivelActual;
+    int puntos;
+    int idMapa;
+    int idJugador;
 
+    public Partida(){
 
+    }
+    public Partida (int idPartida, int nivelActual, int puntos, int idMapa, int idJugador){
+        this();
+        this.setIdPartida(idPartida);
+        this.setNivelActual(nivelActual);
+        this.setPuntos(puntos);
+        this.setIdMapa(idMapa);
+        this.setIdJugador(idJugador);
+
+    }
+    public int getIdPartida() {
+        return idPartida;
+    }
+
+    public void setIdPartida(int idPartida) {
+        this.idPartida = idPartida;
+    }
+
+    public int getNivelActual() {
+        return nivelActual;
+    }
+
+    public void setNivelActual(int nivelActual) {
+        this.nivelActual = nivelActual;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public int getIdMapa() {
+        return idMapa;
+    }
+
+    public void setIdMapa(int idMapa) {
+        this.idMapa = idMapa;
+    }
+
+    public int getIdJugador() {
+        return idJugador;
+    }
+
+    public void setIdJugador(int idJugador) {
+        this.idJugador = idJugador;
+    }
 }
